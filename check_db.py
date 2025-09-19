0b4dc23700b75f26996aa3934093dd724975295f
erioluwabus-glitch <erioluwabus@gmail.com>
2025-09-19 21:12:19 +0100

    Update check_db.py

--- check_db.py
+++ check_db.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect('C:/Users/ERI DANIEL/Desktop/avap_bot/student_data.db')
+conn = sqlite3.connect('C:/Users/ERI DANIEL/Desktop/avap_bot/')
 cursor = conn.cursor()
 cursor.execute("SELECT code, telegram_id, username, question, chat_id, status FROM questions")
 print(cursor.fetchall())
